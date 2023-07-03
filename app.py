843634e46c6d1497a2cf1023fdc0b84598ed83ec
sagar <ssapps.sagar@gmail.com>
2023-07-03 14:45:27 +0530

    db

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/template", methods=["GET","POST"])
+def template():
+    if request.method == "POST":
+        pass
+    else:
+     return render_template("template.html")
+
 
 if __name__ == '__main__':
     app.run()
